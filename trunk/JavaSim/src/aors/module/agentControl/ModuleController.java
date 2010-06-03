29906139a885f5e8f9044653a3cb1648ec7d7d72
dimircea <dimircea@e1dbdc76-492f-11de-acc5-7fb027fc3da9>
2010-06-03 08:31:41 +0000

    UPDATE: agent control libraries loading on running in non-eclipse environment.

--- trunk/JavaSim/src/aors/module/agentControl/ModuleController.java
+++ trunk/JavaSim/src/aors/module/agentControl/ModuleController.java
@@ -10,7 +10,10 @@ import aors.model.agtsim.AgentSubject;
 import aors.model.envevt.EnvironmentEvent;
 import aors.module.Module;
 import aors.module.agentControl.gui.GUIManager;
+import aors.util.jar.JarUtil;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -28,18 +31,47 @@ public class ModuleController implements Module {
 	private static ModuleController instance = null;
 
 	public ModuleController() {
-		if(instance != null) {
+		if (instance != null) {
 			instance.reset();
 		}
 		ModuleController.instance = this;
 
 		this.gui = new GUIManager(this);
 		this.projectDirectory = null;
-	
-		this.agentControllers = new HashMap<Boolean,
-			Set<AgentController<? extends AgentSubject>>>();
+
+		this.agentControllers = new HashMap<Boolean, Set<AgentController<? extends AgentSubject>>>();
 		this.initIdentifier = true;
 		this.controlledAgent = null;
+
+		// initialize module libraries
+		initModuleLibraries();
+	}
+
+	/**
+	 * Initialize the module libraries by unpacking the Jars, loading and
+	 * setting class paths.
+	 */
+	private void initModuleLibraries() {
+		// local path in the temporarily directory for this module
+		String localTmpPath = "agentControlModule";
+
+		// path to jar
+		String jarPath = System.getProperty("user.dir") + File.separator
+				+ "modules" + File.separator + "agentControlModule.jar";
+
+		// extract the jar files for sound module
+		try {
+			JarUtil.extractFileFromJar(jarPath, localTmpPath, "lib",
+					"core-renderer-r8.jar");
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+		// add this path in the library path...
+		JarUtil.setLibraryPath(localTmpPath);
+
+		// load jars from that temporarily directory
+		JarUtil.loadJar(localTmpPath, "core-renderer-r8.jar");
 	}
 
 	public ModuleController(Module vizualisationModule) {
@@ -70,7 +102,6 @@ public class ModuleController implements Module {
 		gui.setSelectionView();
 	}
 
-
 	@Override
 	public void simulationEnded() {
 		this.initIdentifier = !this.initIdentifier;
@@ -79,31 +110,32 @@ public class ModuleController implements Module {
 
 	@Override
 	public void simulationStepStart(long stepNumber) {
-		if(this.controlledAgent != null) {
+		if (this.controlledAgent != null) {
 			controlledAgent.updateView();
 		}
 	}
 
 	@Override
 	public void simulationStepEnd(SimulationStepEvent simulationStepEvent) {
-//		if(controlledAgent != null) {
-//			controlledAgent.performUserActions();
-//		}
-//		System.out.println("=======");
+		// if(controlledAgent != null) {
+		// controlledAgent.performUserActions();
+		// }
+		// System.out.println("=======");
 	}
 
 	@Override
-	public void simulationDomOnlyInitialization(SimulationDescription simulationDescription) {
+	public void simulationDomOnlyInitialization(
+			SimulationDescription simulationDescription) {
 	}
 
 	@Override
-	public void simulationEnvironmentEventOccured(EnvironmentEvent environmentEvent) {
+	public void simulationEnvironmentEventOccured(
+			EnvironmentEvent environmentEvent) {
 	}
 
 	@Override
 	public void simulationInfosEvent(SimulationEvent simulationEvent) {
 	}
-
 
 	@Override
 	public void simulationPaused(boolean pauseState) {
@@ -114,7 +146,6 @@ public class ModuleController implements Module {
 		this.projectDirectory = projectDirectory;
 	}
 
-
 	@Override
 	public void objektDestroyEvent(ObjektDestroyEvent objektDestroyEvent) {
 	}
@@ -123,13 +154,12 @@ public class ModuleController implements Module {
 	public void objektInitEvent(ObjektInitEvent objInitEvent) {
 	}
 
-
 	public void setControlledAgent(long id) {
 		this.controlledAgent = null;
 
-		for(AgentController<? extends AgentSubject> controller :
-			this.agentControllers.get(initIdentifier)) {
-			if(controller.getSubject().getId() == id) {
+		for (AgentController<? extends AgentSubject> controller : this.agentControllers
+				.get(initIdentifier)) {
+			if (controller.getSubject().getId() == id) {
 				this.controlledAgent = controller;
 				this.gui.setControlView(this.controlledAgent);
 				this.controlledAgent.setAgentIsControlled(true);
@@ -139,17 +169,17 @@ public class ModuleController implements Module {
 	}
 
 	public String getProjectPath() {
-		if(projectDirectory != null) {
+		if (projectDirectory != null) {
 			return projectDirectory.getPath();
 		}
 		return null;
 	}
 
-	public void addAgentController(AgentController<? extends AgentSubject>
-		agentController) {
-		if(!this.agentControllers.containsKey(initIdentifier)) {
+	public void addAgentController(
+			AgentController<? extends AgentSubject> agentController) {
+		if (!this.agentControllers.containsKey(initIdentifier)) {
 			this.agentControllers.put(initIdentifier,
-				new HashSet<AgentController<? extends AgentSubject>>());
+					new HashSet<AgentController<? extends AgentSubject>>());
 		}
 		this.agentControllers.get(initIdentifier).add(agentController);
 	}
