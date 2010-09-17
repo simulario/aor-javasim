6b1ac5cf535790dca96d20336498d8effbf57882
gwagner57 <gwagner57@e1dbdc76-492f-11de-acc5-7fb027fc3da9>
2010-09-17 09:54:41 +0000

    Added new utility classes in util/economics

---
+++ trunk/JavaSim/src/aors/util/economics/ContinuousQuantity.java
@@ -0,0 +1,85 @@
+package aors.model.util.economics;
+
+/**
+ * The datatype class ContinuousQuantity allows to define continuous quantites 
+ * (such as 2.3 liters of water, 2 kJ of energy or 7.5 hours of labor) by
+ * creating a suitable continuous quantity type (such as Water, Energy or Labor) 
+ * as an instance of the datatype class ContinuousQuantityType and then creating
+ * an instance of ContinuousQuantity while setting its type property to that type.
+ * 
+ * Author: Gerd Wagner (3-Sept-2010)
+ *
+ * ISSUES
+ * - should this class implement Cloneable?
+ */
+public class ContinuousQuantity {
+
+  private double quantity;  
+  private ContinuousQuantityType continuousQuantityType;  
+  private String unit;  // optional / ISSUE: better define as a suitable enum?
+    
+  /**
+   * Construct a continuous quantity with a default unit
+   */
+  public ContinuousQuantity( double q, ContinuousQuantityType t) {
+    this.quantity = q;
+    this.continuousQuantityType = t;
+  }    
+  /**
+   * Construct a continuous quantity with a non-default unit
+   */
+  public ContinuousQuantity( double q, ContinuousQuantityType t, String u) {
+    this.quantity = q;
+    this.continuousQuantityType = t;
+    this.unit = u;
+  }    
+
+  // Implements abstract supertype method.
+  public double getQuantity() {
+    return quantity;
+  }
+  public String getUnit() {
+    return unit;
+  }
+  public ContinuousQuantityType getContinuousQuantityType() {
+    return continuousQuantityType;
+  }
+  
+  /**
+   * Returns false, since a continuous quantity is a non-discrete commodity.
+   */
+  public final boolean isDiscrete() { 
+    return false;
+  }
+  /**
+   * Returns the type as a string.
+   */
+  public String getType() {
+    return this.continuousQuantityType.getName();
+  }
+  /**
+   * Add an amount of this type of continuous quantity. A RuntimeException will be 
+   * thrown if amount is negative.
+   */
+  public void deposit( double amount) {
+    if (amount < 0) throw new RuntimeException("Amount must be >= 0");
+    quantity += amount;
+  }
+  /**
+   * Remove an amount of this type of continuous quantity. If quantity is smaller
+   * than amount, quantity is set to 0. Returns the amount successfully 
+   * withdrawn. A RuntimeException will be thrown if amount is negative. 
+   */
+  public double withdraw( double amount) {
+    if (amount < 0) throw new RuntimeException("Amount must be >= 0");
+    if (amount <= quantity) {
+      quantity -= amount;
+      return amount;
+    }
+    else {
+      double oldQuantity = quantity;
+      quantity = 0;
+      return oldQuantity;
+    }
+  }
+}
