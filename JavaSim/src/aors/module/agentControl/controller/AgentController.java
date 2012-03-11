4c1d66bbf05163e2be34517bbfcde44761cea101
dimircea <dimircea@e1dbdc76-492f-11de-acc5-7fb027fc3da9>
2012-03-11 16:25:09 +0000

    UPDATE: drop initial state module and agent control module - impossible to use them since are completely out of date.

--- JavaSim/src/aors/module/agentControl/controller/AgentController.java
+++
@@ -1,5 +0,0 @@
-package aors.module.agentControl.controller;
-
-import aors.model.agtsim.agentControl.CoreAgentController;
-
-public interface AgentController extends CoreAgentController, ModuleAgentController {}
